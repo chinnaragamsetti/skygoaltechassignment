cea5ef8c6a88b7bfbf8275c1050e1129f3f508e2
chinnaragamsetti <ragamsettirajkumar@gmail.com>
2024-05-26 11:15:00 +0000

    26-5-24

---
+++ app.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const app = express();
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "skyGoalTechApp.db");
+
+let db = null;
+
+const startServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM userDetails WHERE username = '${username}';`;
+  const dbUser = await db.get(selectUserQuery);
+  //  console.log(dbUser);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      const payload = {
+        username: username,
+      };
+      const jwtToken = jwt.sign(payload, "rajkumar@1");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
+  }
+});
+
+const authenticateToken = (request, response, next) => {
+  let jwtToken;
+  // console.log(request.headers);
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid JWT Token");
+  } else {
+    jwt.verify(jwtToken, "rajkumar@1", async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        request.username = payload.username;
+        next();
+      }
+    });
+  }
+};
+
+app.get("/userDetails/", authenticateToken, async (request, response) => {
+  const getStatesQuery = `
+  SELECT
+    *
+  FROM
+    userDetails;`;
+  const userDetailsArray = await db.all(getStatesQuery);
+  response.send(
+    userDetailsArray.map((each) => ({
+      Id: each.id,
+      userName: each.name,
+      userPassword: each.password,
+    }))
+  );
+});
